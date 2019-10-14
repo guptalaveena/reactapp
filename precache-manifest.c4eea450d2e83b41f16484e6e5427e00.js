54a3b038930f76b623263192ecc689358aaf0ba6
laveena-gupta-vst-au4 <54509277+laveena-gupta-vst-au4@users.noreply.github.com>
2019-10-14 23:30:59 +0530

    Updates

---
+++ precache-manifest.c4eea450d2e83b41f16484e6e5427e00.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "22a663ac324f7965c31934f642d820bb",
+    "url": "/reactapp/index.html"
+  },
+  {
+    "revision": "179df9f0958861d5f2d6",
+    "url": "/reactapp/static/css/main.ebe54236.chunk.css"
+  },
+  {
+    "revision": "566553a069b20db4ff30",
+    "url": "/reactapp/static/js/2.62672036.chunk.js"
+  },
+  {
+    "revision": "179df9f0958861d5f2d6",
+    "url": "/reactapp/static/js/main.8a66ef62.chunk.js"
+  },
+  {
+    "revision": "4b12244fde303f690206",
+    "url": "/reactapp/static/js/runtime-main.22ca1bae.js"
+  }
+]);
